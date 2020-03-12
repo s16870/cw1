203c3aa5f75f0dbf4b6e3470dbc07bc859b1f5c2
s16870@pjwstk.edu.pl <s16870@pjwstk.edu.pl>
2020-03-12 15:54:47 +0100

    Pierwszy commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            HttpResponseMessage res = await httpClient.GetAsync("https://www.pja.edu.pl/dziekanat");
+
+            if (res.IsSuccessStatusCode)
+            {
+                string html = await res.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(html);
+                Console.WriteLine(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+            }
+
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
